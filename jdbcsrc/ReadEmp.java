a950bf3e217e9b4ede776c2566b52c2afe444687
Jeong-hae-rim <58800295+Jeong-hae-rim@users.noreply.github.com>
2019-12-31 17:36:35 +0900

    Do-While Use

--- jdbcsrc/ReadEmp.java
+++ jdbcsrc/ReadEmp.java
@@ -30,17 +30,16 @@ public class ReadEmp {
 		ResultSet rs = stmt.executeQuery(sql);
 		
 	    if(rs.next()) {
-	    	System.out.println(rs.getString("ename") + ":" +
-                               rs.getInt("sal") + ":" +
-			                   rs.getInt("deptno"));
-	    	while(rs.next()) {
-			System.out.println(rs.getString("ename") + ":" +
-		                       rs.getInt("sal") + ":" +
-					           rs.getInt("deptno"));
-	    	}
+	    	do {
+	    		System.out.println(rs.getString("ename") + ":" +
+	                               rs.getInt("sal") + ":" +
+				                   rs.getInt("deptno"));
+	    	}while(rs.next());
 		} else {
 			System.out.println(num + "번 부서에 근무하는 직원이 없습니다.");
 		}
+	    
+	    //한 번 처리하고 나서 반복 처리시키고 싶을 때 do-while문
 		
 		System.out.println("-----------------끝----------");
 		rs.close();
