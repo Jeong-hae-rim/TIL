22560340d8122133303e1db0ab08e3dcc5c0920d
Jeong-hae-rim <58800295+Jeong-hae-rim@users.noreply.github.com>
2020-01-07 13:26:00 +0900

    fixed

--- javalearning/WhileLab2.java
+++ javalearning/WhileLab2.java
@@ -4,28 +4,22 @@ public class WhileLab2 {
 
 	public static void main(String[] args) {
 
-		int pairNum1 = (int) (Math.random() * 6) + 1;
-		int pairNum2 = (int) (Math.random() * 6) + 1;
-
-		
-		while(true) {
+		while (true) {
+			int pairNum1 = (int) (Math.random() * 6) + 1;
+			int pairNum2 = (int) (Math.random() * 6) + 1;
 			if (pairNum1 > pairNum2) {
 				System.out.println(pairNum1 + "이 " + pairNum2 + "보다 크다.");
-			} 
-			else if (pairNum1 < pairNum2) {
+			} else if (pairNum1 < pairNum2) {
 				System.out.println(pairNum1 + "이 " + pairNum2 + "보다 작다.");
-			} 
-			else {
+			} else {
 				System.out.println("게임 끝.");
-			
-				
+
+				break;
+
 			}
-			
-			break;
 
 		}
 
 	}
 
 }
-
